7006b1631cc38935b002deb1a23c6e846d82b6be
Alberto Mercurio <alberto.mercurio96@gmail.com>
2025-02-11 11:03:19 +0100

    time-independent evolution completed

--- src/time_evolution.jl
+++ src/time_evolution.jl
@@ -53,7 +53,7 @@ a = tensor(destroy(N), qeye(2)) # Annihilation operator of the cavity
 H = ωc * a' * a + ωa / 2 * σz + g * (a * σp + a' * σm)
 
 # %% [markdown]
-# We now perform the time evolution of the system.
+# The time evolution can be simply simulated by
 
 # %%
 ψ0 = tensor(fock(N, 0), basis(2, 0)) # Initial state: vacuum state of the cavity and excited state of the atom
@@ -68,11 +68,11 @@ res_se = sesolve(H, ψ0, tlist, e_ops=e_ops)
 #
 # As can be seen from @fig-se-me-mc-solve-1, the number of photons oscillates between the cavity and the atom, showing the vacuum Rabi oscillations.
 # 
-# Its is important to note that the memory allocated does not depend on the number of points in the time grid or the final time. This is because the solver uses the most efficient algorithms given by the [*DifferentialEquations.jl*](https://github.com/SciML/DifferentialEquations.jl?tab=readme-ov-file) package [@christopher_rackauckas_2024_differentialequations], computing intermediate steps in-place. It only allocates extra memory if the user requests the storage of the states with the `saveat` keyword argument, specifying the times at which the states are saved.
+# Its is important to note that the memory allocated does not depend on the number of points in the time grid or the final time. This is because the solver uses the most efficient algorithms given by the [*DifferentialEquations.jl*](https://github.com/SciML/DifferentialEquations.jl?tab=readme-ov-file) package [@christopher_rackauckas_2024_differentialequations] [@RackauckasDifferentialEquations_jl2017], computing intermediate steps in-place. It only allocates extra memory if the user requests the storage of the states with the `saveat` keyword argument, specifying the times at which the states are saved.
 
 # ## Master Equation
 
-# The Schrödinger equation is a unitary evolution equation, which describes the dynamics of a closed quantum system. However, in many cases, the system is not isolated, as it interacts with an environment. In this case, the dynamics of the system is described by the Lindblad master equation
+# The Schrödinger equation is a unitary evolution equation, which describes the dynamics of a closed quantum system. However, in many cases, the system is not isolated, as it interacts with an environment. In the case the system is weakly interacting with a Markovian environment, the dynamics of the system is described by the Lindblad master equation
 
 # $$
 # \frac{d}{dt}\hat{\rho} = -i [\hat{H}, \hat{\rho}] + \sum_k \mathcal{D}[\hat{C}_k] \hat{\rho} \, ,
@@ -81,7 +81,7 @@ res_se = sesolve(H, ψ0, tlist, e_ops=e_ops)
 # where $\hat{\rho}$ is the density matrix of the system, $\hat{H}$ is the Hamiltonian operator, $\hat{C}_k$ are the collapse operators, and 
 
 # $$
-# \mathcal{D}[\hat{C}] \hat{\rho} = \hat{C} \hat{\rho} \hat{C}^\dagger - \frac{1}{2} \{ \hat{C}^\dagger \hat{C}, \hat{\rho} \}
+# \mathcal{D}[\hat{C}] \hat{\rho} = \hat{C} \hat{\rho} \hat{C}^\dagger - \frac{1}{2} \left( \hat{C}^\dagger \hat{C} \hat{\rho} - \hat{\rho} \hat{C}^\dagger \hat{C} \right)
 # $$
 
 # is the Lindblad dissipator. *QuantumToolbox.jl* provides the [`mesolve`](https://qutip.org/QuantumToolbox.jl/stable/resources/api#QuantumToolbox.mesolve) function to solve the master equation for a given Hamiltonian, initial state, and collapse operators.
@@ -107,6 +107,8 @@ res_me = mesolve(H, ψ0, tlist, c_ops, e_ops=e_ops)
 
 # %% [markdown]
 # @fig-se-me-mc-solve-2 shows the expectation value of the number operator $\langle \hat{a}^\dagger \hat{a} \rangle$ as a function of time. Contrary to the Schrödinger equation, the cavity population decays exponentially due to the cavity decay rate $\kappa$. Indeed, the dashed gray line represents the exponential decay of the cavity population due to the cavity decay rate $\kappa$. The master equation solver is able to capture this behavior, as well as the oscillations due to the atom-cavity coupling.
+#
+# When only the steady state is of interest, the [`steadystate`](https://qutip.org/QuantumToolbox.jl/stable/resources/api#QuantumToolbox.steadystate) function can be used to directly compute the steady state, without the need to solve the master equation in time. This function allows several algorithms such as direct, iterative, and eigenvalue-based methods [@Nation2015Iterative][@Nation2015Steady_state].
 
 # ## Monte Carlo Quantum Trajectories
 
@@ -119,7 +121,7 @@ res_me = mesolve(H, ψ0, tlist, c_ops, e_ops=e_ops)
 # Being the evolution non-unitary, the norm of the state is not conserved, decreasing in time due to the dissipation. A quantum jump occurs when the norm of the state reaches a random threshold, previously sampled from a uniform distribution between 0 and 1. If many jump operators are present, the probability of each jump is equal to
 
 # $$
-# P_k = \frac{\langle \psi(t) | \hat{C}_k^\dagger \hat{C}_k | \psi(t) \rangle}{\sum_j \langle \psi(t) | \hat{C}_j^\dagger \hat{C}_j | \psi(t) \rangle} \, .
+# P_k (t) = \frac{\langle \psi(t) | \hat{C}_k^\dagger \hat{C}_k | \psi(t) \rangle}{\sum_j \langle \psi(t) | \hat{C}_j^\dagger \hat{C}_j | \psi(t) \rangle} \, .
 # $$
 
 # As an example, let's consider the same system as before, where both the cavity and the atom are coupled to a zero-temperature bath. In addition to other keyword arguments, we also use the `ntraj` and `rng` arguments to specify the number of quantum trajectories and the random seed, respectively. The second argument is optional, but it is useful for reproducibility.
