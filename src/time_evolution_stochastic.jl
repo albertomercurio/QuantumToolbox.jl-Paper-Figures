d833cd352163dc0d161b8406c0802d7276673cd0
Alberto Mercurio <alberto.mercurio96@gmail.com>
2025-04-08 15:41:36 +0200

    Sotchastic time evolution completed

--- src/time_evolution_stochastic.jl
+++ src/time_evolution_stochastic.jl
@@ -1,5 +1,4 @@
 using QuantumToolbox
-# using StochasticDiffEq
 using CairoMakie
 
 include("_cairomakie_setup.jl")
@@ -7,38 +6,43 @@ include("_cairomakie_setup.jl")
 # %%
 
 # parameters
-N = 20         # Fock space dimension
-ω = 2π * 5     # cavity resonance frequency
-κ = 2          # cavity decay rate
-n = 4          # intensity of initial state
-ntraj = 1000    # number of trajectories
+N = 20      # Fock space dimension
+ωc = 5      # cavity resonance frequency
+ωq = 5      # qubit resonance frequency
+g = 0.1     # coupling strength
+κ = 1       # cavity decay rate
+γ = 2       # qubit decay rate
+α = 2       # coherence of initial state
+ntraj = 500 # number of trajectories
 
 # %%
 
-tlist = range(0, 1.5, 400)
+tlist = range(0, 10π / ωc, 400)
 
 # operators
-a = destroy(N)
-H = ω * a' * a
+a = tensor(destroy(N), qeye(2))
+σm = tensor(qeye(N), sigmam())
+σz = tensor(qeye(N), sigmaz())
+
+H = ωc * a' * a + ωq * σz / 2 + g * (a' * σm + a * σm')
 
 # initial state
-ψ0 = coherent(N, √n)
+ψ0 = tensor(coherent(N, α), basis(2, 0)) # |α, e⟩
 # %%
 
-# collapse operators
+# stochastic collapse operators
 sc_ops = [√(κ) * a]
 
-x = (a + a') * √(κ)
+X = (a + a') * √(κ)
 
 sse_sol = ssesolve(
     H,
     ψ0,
     tlist,
     sc_ops[1],
-    e_ops = [x],
+    e_ops = [X],
     ntraj = ntraj,
     store_measurement = Val(true),
-    # abstol=1e-3,
 )
 
 measurement_sse_avg = sum(sse_sol.measurement, dims=2) / size(sse_sol.measurement, 2)
@@ -46,12 +50,8 @@ measurement_sse_avg = dropdims(measurement_sse_avg, dims=2)
 
 # %%
 
-# temperature with average of 1 excitations
-n_th = 1
-c_ops  = [√(κ * n_th) * a']
-sc_ops = [√(κ * (n_th + 1)) * a]
-
-x_sme = (a + a') * √(κ * (n_th + 1))
+# Including qubit losses
+c_ops  = [√(γ) * σm]
 
 sme_sol = smesolve(
     H,
@@ -59,7 +59,7 @@ sme_sol = smesolve(
     tlist,
     c_ops,
     sc_ops[1],
-    e_ops = [x_sme],
+    e_ops = [X],
     ntraj = ntraj,
     store_measurement = Val(true)
 )
@@ -71,8 +71,8 @@ measurement_sme_avg = dropdims(measurement_sme_avg, dims=2)
 
 fig = Figure(size = (plot_figsize_width_single_column_pt, 1.2 * plot_figsize_width_single_column_pt))
 
-ax_sse = Axis(fig[1, 1], ylabel="Measurement")
-ax_sme = Axis(fig[2, 1], ylabel="Measurement", xlabel = "Time")
+ax_sse = Axis(fig[1, 1], ylabel="Measurement", yticks = [-5, 0, 5])
+ax_sme = Axis(fig[2, 1], ylabel="Measurement", xlabel = "Time", yticks = [-5, 0, 5])
 
 lines!(ax_sse, tlist[1:end-1], real(measurement_sse_avg[1,:]), label = L"J_x", color = :lightsteelblue)
 lines!(ax_sse, tlist, real(sse_sol.expect[1,:]),  label = L"\langle \hat{X} \rangle", color = :dodgerblue4)
@@ -85,8 +85,8 @@ axislegend(ax_sse, position = :rt, padding = 0)
 
 xlims!(ax_sse, tlist[1], tlist[end])
 xlims!(ax_sme, tlist[1], tlist[end])
-ylims!(ax_sse, -11, 11)
-ylims!(ax_sme, -11, 11)
+ylims!(ax_sse, -6, 6)
+ylims!(ax_sme, -6, 6)
 
 # -- DECORATION --
 hidexdecorations!(ax_sse, ticks=false)
