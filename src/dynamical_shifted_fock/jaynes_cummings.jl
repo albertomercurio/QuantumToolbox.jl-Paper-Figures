c002f85e761072af8e8308af7869c0da321e7b1a
Alberto Mercurio <alberto.mercurio96@gmail.com>
2025-08-23 23:48:21 +0200

    New simulation of DSF with jaynes Cummings

--- src/dynamical_shifted_fock/jaynes_cummings.jl
+++ src/dynamical_shifted_fock/jaynes_cummings.jl
@@ -77,6 +77,9 @@
 # %%
 using LinearAlgebra
 using QuantumToolbox
+using QuantumToolbox: Tsit5
+using QuantumCumulants
+using ModelingToolkit
 using CairoMakie
 
 include("../_cairomakie_setup.jl")
@@ -88,19 +91,20 @@ include("../_cairomakie_setup.jl")
 # %%
 # Parameters
 
-const F   = 7
+const F   = 5.0
 const Δc  = 0.0
 const Δa  = -0.3
-const γ   = 1
-const U = 0.002
-const g = 3
+const γc   = 1.0
+const γa   = 0.01
+const U   = -0.002
+const g   = 3.0
 
 const N0 = 200
 
-const a0 = tensor(destroy(N0), qeye(2))
+const a0 = kron(destroy(N0), qeye(2))
 
-const σm0 = tensor(qeye(N0), sigmam())
-const σz0 = tensor(qeye(N0), sigmaz())
+const σm0 = kron(qeye(N0), sigmam())
+const σz0 = kron(qeye(N0), sigmaz())
 
 # %% [markdown]
 # Full Master Equation
@@ -108,9 +112,9 @@ const σz0 = tensor(qeye(N0), sigmaz())
 # %%
 
 H0     = Δc*a0'*a0 + Δa/2 * σz0 + U*(a0^2)'*a0^2 + g*(a0*σm0' + a0'*σm0) + F*(a0+a0')
-c_ops0 = [√γ*a0, √γ*σm0]
+c_ops0 = [√γc*a0, √γa*σm0]
 
-ψ0 = tensor(fock(N0, 0), basis(2, 1))
+ψ0 = kron(fock(N0, 0), basis(2, 1))
 
 tlist = range(0, 15/γ, 1000)
 tlist_save = [tlist[1], 2.2, 0.9*tlist[end]]
@@ -128,7 +132,7 @@ end
 
 function c_ops_dsf(op_l, p)
     a = op_l[1]
-    return [√γ*a, √γ*σm]
+    return [√γc * a, √γa * σm]
 end
 
 function e_ops_dsf(op_l, p)
@@ -136,20 +140,62 @@ function e_ops_dsf(op_l, p)
     return [a'*a, a]
 end
 
-const N_dsf = 15
+const N_dsf = 13
 
-const a = tensor(destroy(N_dsf), qeye(2))
-const σm = tensor(qeye(N_dsf), sigmam())
-const σz = tensor(qeye(N_dsf), sigmaz())
+const a = kron(destroy(N_dsf), qeye(2))
+const σm = kron(qeye(N_dsf), sigmam())
+const σz = kron(qeye(N_dsf), sigmaz())
 
 # %%
 
 op_l = [a]
 α0_l = [expect(a0, ψ0)]
 
-ψ0_dsf = tensor(fock(N_dsf, 0), basis(2, 1))
+ψ0_dsf = kron(fock(N_dsf, 0), basis(2, 1))
 
 sol_dsf = dsf_mesolve(H_dsf, ψ0_dsf, tlist, c_ops_dsf, op_l, α0_l, e_ops=e_ops_dsf, krylov_dim=5)
+
+# %% [markdown]
+
+## Quantum Cumulants
+
+# %%
+
+# Define hilbert space
+h1 = FockSpace(:cavity1)
+ha = NLevelSpace(:atom,(:g,:e))
+h = QuantumCumulants.tensor(h1, ha)
+
+# Define the fundamental operators
+@qnumbers a₂::Destroy(h,1)
+σm₂ = Transition(h,:σ,:g,:e)
+σz₂ = σm₂'*σm₂ - σm₂*σm₂'
+@cnumbers F₂ Δc₂ Δa₂ U₂ γc₂ γa₂ g₂
+
+# Hamiltonian
+H₂ = Δc₂ * a₂'*a₂ + Δa₂/2 * σz₂ + U₂*(a₂^2)'*a₂^2 + g₂*(a₂*σm₂' + a₂'*σm₂) + F₂*(a₂+a₂')
+
+# Collapse operators
+Jumps₂ = [a₂, σm₂]
+rates₂ = [γc₂, γa₂]
+
+# Derive a set of equations
+ops = [a₂'*a₂, σm₂' * σm₂]
+eqs = meanfield(ops,H₂,Jumps₂;rates=rates₂,order=2)
+
+# Expand the above equations to second order
+eqs_expanded = ModelingToolkit.complete(eqs);
+
+# Generate an ODESystem
+@named sys = ODESystem(eqs_expanded);
+
+# Solve the system using the OrdinaryDiffEq package
+u0 = zeros(ComplexF64, length(eqs_expanded))
+
+p = (F₂, Δc₂, Δa₂, U₂, γc₂, γa₂, g₂)
+p0 = p .=> (F, Δc, Δa, U, γc, γa, g)
+prob = ODEProblem(sys, u0,(tlist[1],tlist[end]),p0,saveat=tlist, abstol=1e-8, reltol=1e-6)
+sol_qc = solve(prob, Tsit5());
 
 # %% [markdown]
 # We now find the last diagonal element of $\hat{\rho}_{t_1}$ that is larger than $10^{-4}$, that would be the selected Hilbert space dimension. The radius of the circle to draw will be the square root of that index.
@@ -231,25 +277,14 @@ ax4 = Axis(fig[2, 3], xticks=-10:10:10, yticks=-10:10:10, xminorticksvisible=tru
 
 lines!(ax1, tlist, real.(sol.expect[1, :]), label="Full", color=full_case_color)
 lines!(ax1, tlist, real.(sol_dsf.expect[1, :]), label="DSF", linestyle=:dash, color=dsf_case_color)
-# lines!(ax1, sol_qc.t, real.(sol_qc[a₁'*a₁]), label="QC", linestyle=:dashdot, color=qc_case_color)
-# lines!(ax1, tlist, abs2.(sol.expect[2, :]))
-# vlines!(ax1, [2.6], color=:red)
-scatter!(ax1, tlist_save, real.(expect(a0'*a0, sol.states)), color=:grey, markersize=5, marker=:rect)
-lines!(ax1, fill(tlist_save[1], 2), [0, real(expect(a0'*a0, sol.states[1]))], color=:grey, linestyle=:dash, linewidth=1)
-lines!(ax1, fill(tlist_save[2], 2), [0, real(expect(a0'*a0, sol.states[2]))], color=:grey, linestyle=:dash, linewidth=1)
-lines!(ax1, fill(tlist_save[3], 2), [0, real(expect(a0'*a0, sol.states[3]))], color=:grey, linestyle=:dash, linewidth=1)
+lines!(ax1, sol_qc.t, real.(sol_qc[a₂'*a₂]), label="QC", linestyle=:dashdot, color=qc_case_color)
 
 axislegend(ax1, orientation=:horizontal, position=:cb, padding=0)
 
 
-# heatmap!(ax2, xvec, yvec, Array(wigner(fock(N0, 70), xvec_gpu, yvec_gpu))', rasterize=true, colorrange=(-vmax1, vmax1), colormap=:balance, interpolate=true)
 heatmap!(ax2, xvec, yvec, wig1', rasterize=2, colorrange=(-vmax1, vmax1), colormap=:balance, interpolate=true)
 heatmap!(ax3, xvec, yvec, wig2', rasterize=2, colorrange=(-vmax2, vmax2), colormap=:balance, interpolate=true)
 heatmap!(ax4, xvec, yvec, wig3', rasterize=2, colorrange=(-vmax3, vmax3), colormap=:balance, interpolate=true)
-
-# contour!(ax2, xvec, yvec, wig1', levels=25, colorrange=(-vmax1, vmax1), colormap=:balance)
-# contour!(ax3, xvec, yvec, wig2', levels=25, colorrange=(-vmax2, vmax2), colormap=:balance)
-# contour!(ax4, xvec, yvec, wig3', levels=25, colorrange=(-vmax3, vmax3), colormap=:balance)
 
 arc!(ax2, Point2(0, 0), α_max_full, 0, 2π, color=full_case_color)
 arc!(ax3, Point2(0, 0), α_max_full, 0, 2π, color=full_case_color)
@@ -291,30 +326,31 @@ text!(ax4, 0, 1, text = "(d)", align = (:left, :top), offset = (2, -1), space = 
 
 poly_kwargs = (color=cgrad(:balance)[128], strokecolor=:black, strokewidth=0.5, linestyle=:solid, alpha=1)
 
+snapshot_point1 = posFig(ax1, tlist_save[1], 0)
+snapshot_point2 = posFig(ax1, tlist_save[2], 0)
+snapshot_point3 = posFig(ax1, tlist_save[3], 0)
+
 ax_origin = ax2.scene.viewport[].origin
 ax_widths = ax2.scene.viewport[].widths
-snapshot_point = posFig(ax1, tlist_save[1], 0)
-poly_x = [snapshot_point[1], ax_origin[1], ax_origin[1] + ax_widths[1]]
-poly_y = [snapshot_point[2], ax_origin[2] + ax_widths[1], ax_origin[2] + ax_widths[2]]
+poly_x = [snapshot_point1[1], ax_origin[1], ax_origin[1] + ax_widths[1]]
+poly_y = [snapshot_point1[2], ax_origin[2] + ax_widths[1], ax_origin[2] + ax_widths[2]]
 poly!(fig.scene, collect(zip(poly_x, poly_y)); poly_kwargs...)
 
 ax_origin = ax3.scene.viewport[].origin
 ax_widths = ax3.scene.viewport[].widths
-snapshot_point = posFig(ax1, tlist_save[2], 0)
-poly_x = [snapshot_point[1], ax_origin[1], ax_origin[1] + ax_widths[1]]
-poly_y = [snapshot_point[2], ax_origin[2] + ax_widths[1], ax_origin[2] + ax_widths[2]]
+poly_x = [snapshot_point2[1], ax_origin[1], ax_origin[1] + ax_widths[1]]
+poly_y = [snapshot_point2[2], ax_origin[2] + ax_widths[1], ax_origin[2] + ax_widths[2]]
 poly!(fig.scene, collect(zip(poly_x, poly_y)); poly_kwargs...)
 
 ax_origin = ax4.scene.viewport[].origin
 ax_widths = ax4.scene.viewport[].widths
-snapshot_point = posFig(ax1, tlist_save[3], 0)
-poly_x = [snapshot_point[1], ax_origin[1], ax_origin[1] + ax_widths[1]]
-poly_y = [snapshot_point[2], ax_origin[2] + ax_widths[1], ax_origin[2] + ax_widths[2]]
+poly_x = [snapshot_point3[1], ax_origin[1], ax_origin[1] + ax_widths[1]]
+poly_y = [snapshot_point3[2], ax_origin[2] + ax_widths[1], ax_origin[2] + ax_widths[2]]
 poly!(fig.scene, collect(zip(poly_x, poly_y)); poly_kwargs...)
 
-# translate!(prova_scatter, 0, 0, 1)
+scatter!(fig.scene, [snapshot_point1, snapshot_point2, snapshot_point3]; color=:grey, markersize=8, marker=:rect)
 
-save(joinpath(@__DIR__, "../figures/dynamical_shifted_fock.pdf"), fig, pt_per_unit=1)
+save(joinpath(@__DIR__, "../../figures/dynamical_shifted_fock.pdf"), fig, pt_per_unit=1)
 
 fig
 
